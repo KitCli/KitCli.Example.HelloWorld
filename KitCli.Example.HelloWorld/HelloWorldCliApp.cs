6db73b755f1993681f649dd9a518751076089c9b
Joshua Crowe <joshuaedwardcrowe@icloud.com>
2026-02-04 22:14:57 +0000

    Updated KitCli Version

--- KitCli.Example.HelloWorld/HelloWorldCliApp.cs
+++ KitCli.Example.HelloWorld/HelloWorldCliApp.cs
@@ -1,11 +1,11 @@
-using KitCli.Commands.Abstractions.Io.Outcomes;
+using KitCli.Abstractions.Io;
 using KitCli.Workflow.Abstractions;
 
 namespace KitCli.Example.HelloWorld;
 
 public class HelloWorldCliApp : CliApp
 {
-    public HelloWorldCliApp(ICliWorkflow workflow, ICliCommandOutcomeIo io) : base(workflow, io)
+    public HelloWorldCliApp(ICliWorkflow workflow, ICliIo io) : base(workflow, io)
     {
     }
 }
